8b59b54d1de375490fc1f798cfa31e4e073f5127
nktamzam <48533790+nktamzam@users.noreply.github.com>
2019-03-21 22:54:05 +0100

    Listar en formato HTML

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 
-function dataRequest() {
+let dataRequest = () => {
   var divresultado = document.getElementById('resultado');
   var loading = document.getElementById('loading');
   divresultado.innerHTML = '';
   loading.style.display = 'block';
 
-  axios.get('https://lanbide-node.herokuapp.com/')
+  axios.get('https://lanbide-node.herokuapp.com?limit=10')
     .then(function (response) {
       divresultado.innerHTML = '<p>Respuesta: ' + response.status + ' ' + response.statusText + '</p>';
-      divresultado.innerHTML += '<p><pre>' + JSON.stringify(response.data, null, '\t') + '</p></pre>';
+      datos = response.data.response.payload.result;
+
+      var i;
+      for (i in datos){
+      divresultado.innerHTML += "<dl>";
+      divresultado.innerHTML += "<dt>Name</dt><dd>" + datos[i].name + "</dd>";
+      divresultado.innerHTML += "<dt>Username</dt><dd>" + datos[i].username + "</dd>";
+      divresultado.innerHTML += "<dt>Email</dt><dd>" + datos[i].email + "</dd>";
+      divresultado.innerHTML += "<dt>Avatar</dt><dd>" + datos[i].avatar + "</dd>";
+      divresultado.innerHTML += "</dl>";
+       }
+
     })
     .catch(function (error) {
       divresultado.innerHTML = '<p>Respuesta: <pre>' + error.response.status + ' ' + error.response.statusText + '</pre></p>';
@@ -20,7 +31,7 @@ function dataRequest() {
     });
 };
 
-function postRequest(e) {
+let postRequest = (e) => {
   loading.style.display = 'block';
   var divresultado = document.getElementById('resultado');
   var nombre = document.getElementById('name').value;
